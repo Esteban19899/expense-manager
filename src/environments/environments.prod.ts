2ce0b8c7251001dc45196da511540144856e9e86
Esteban19899 <evillanueva@alumnos.exa.unicen.edu.ar>
2025-09-19 19:37:17 -0300

    change url

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   supabaseUrl: '', // Deja el valor vacío
   supabaseAnonKey: '', // Deja el valor vacío
-  supabaseRedirectUrl: 'https://expense-manager-blue-tau.vercel.app/dashboard' //URL de Vercel
+  supabaseRedirectUrl: 'https://adondesemefue.vercel.app/dashboard' //URL de Vercel
 };
